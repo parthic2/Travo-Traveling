e7ea3a0c53d86989f7e065e724ec0893741ccb63
parthi <Stackholic-Infotech@dev.azure.com>
2023-09-01 16:08:41 +0530

    01-09-23

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             <div className="lg-no-parallax relative w-1/2 md:w-width-70 z-[2] mt-spacing-t lg:mt-0 lg:mr-auto flex justify-center items-center">
               <img src={AboutImage1} alt="about" className="aboutImage-1" loading="lazy" />
             </div>
-            <div className="lg-no-parallax flex justify-center items-center w-width-65 bg-no-repeat absolute bottom-spacing-b right-spacing-right-15 lg:!left-auto lg:!right-0">
+            <div className="lg-no-parallax flex justify-center items-center bg-no-repeat absolute bottom-spacing-b right-spacing-right-15 lg:!left-auto lg:!right-0">
               <img src={AboutImage2} className="aboutImage-2" alt="about" loading="lazy" />
             </div>
           </div>
